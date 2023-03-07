1dae129e9d7f6fe161bebd68a4e18fe69e71bb68
Nitharson Jeyakumar <nitharson18@gmail.com>
2023-03-07 09:20:12 +0100

    Create  carte_pizzeria.py

---
+++ carte_pizzeria.py
@@ -0,0 +1,18 @@
+class CartePizzeria:
+    def __init__(self):
+        self.pizzas = []
+
+    def is_empty(self):
+        return len(self.pizzas) == 0
+
+    def nb_pizzas(self):
+        return len(self.pizzas)
+
+    def add_pizza(self, pizza):
+        self.pizzas.append(pizza)
+
+    def remove_pizza(self, name):
+        for i in range(len(self.pizzas)):
+            if self.pizzas[i].name == name:
+                del self.pizzas[i]
+                return
